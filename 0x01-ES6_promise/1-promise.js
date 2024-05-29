1d87c01933ecd1e858c7a100ba573bf9dafa9be0
oumaima <oumaimanaanaapro@gmail.com>
2024-05-29 20:49:15 +0100

    1-promise.js

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+export default function getFullResponseFromAPI(success) {
+  const objResp = {
+    status: 200,
+    body: 'Success',
+  };
+
+  if (success) {
+    return Promise.resolve(objResp);
+  } else {
+    return Promise.reject(new Error('The fake API is not working currently'))
+  }
+}
