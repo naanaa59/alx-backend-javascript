511b5ae2d327a8b6001b93327b279d76b5986703
oumaima <oumaimanaanaapro@gmail.com>
2024-05-29 20:46:37 +0100

    0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,4 @@
 export default function getResponseFromAPI() {
-  const myPromise = new Promise(function(myResolver, myReject) {
-    let success = true;
-    if (success) {
-      myResolver('Success');
-    } else {
-      myReject('Failure');
-    }
-  });
-  return myPromise;
+  return Promise.resolve(true);
 }
+
