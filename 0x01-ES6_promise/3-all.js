3fba541590b6d61eb8b8583966d8b81d11ef1137
oumaima <oumaimanaanaapro@gmail.com>
2024-05-31 12:38:22 +0100

    add return and fix eslint

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
     })
-  .catch(() => {
-    console.log('Signup system offline')
-  });
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
 
 export default handleProfileSignup;
