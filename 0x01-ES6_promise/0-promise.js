9c2a30d4a40d93192539d2401009d9e351354da6
oumaima <oumaimanaanaapro@gmail.com>
2024-05-31 10:14:06 +0100

    fix eslint

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,4 @@
-export default function getResponseFromAPI() {
-  const myPromise = new Promise(function(myResolver, myReject) {
-    let success = true;
-    if (success) {
-      myResolver('Success');
-    } else {
-      myReject('Failure');
-    }
-  });
-  return myPromise;
+function getResponseFromAPI() {
+  return Promise.resolve('Success');
 }
+export default getResponseFromAPI;
