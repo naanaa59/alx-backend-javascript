e9a9c51f3a4762893aca3840668b229dd12c5e58
oumaima <oumaimanaanaapro@gmail.com>
2024-05-31 12:35:13 +0100

    3 add reject result

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,10 @@ function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
-    });
+    })
+  .catch(() => {
+    console.log('Signup system offline')
+  });
 }
 
 export default handleProfileSignup;
