de15f74690c8761f58e8b35043f25d2b110edfc0
oumaima <oumaimanaanaapro@gmail.com>
2024-06-02 12:44:38 +0100

    4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-underscore-dangle */
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount === 'number') {
+      this._amount = amount;
+    } else {
+      throw new TypeError('amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    }
+  }
+
+  displayFullPrice() {
+    // amount currency_name (currency_code)
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('amount and conversionRate must be numbers');
+    } else {
+      return amount * conversionRate;
+    }
+  }
+}
