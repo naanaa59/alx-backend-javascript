7fea2b16783bcb411ec63ac6723f202a6bccb2a8
oumaima <oumaimanaanaapro@gmail.com>
2024-06-05 20:58:49 +0100

    8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, startString) {
+  const seperator = '-';
+  if (startString === '') {
+    return '';
+  }
+  return Array.from(set)
+    .filter((str) => str.includes(startString))
+    .map((str) => str.replace(startString, ''))
+    .join(seperator);
+}
