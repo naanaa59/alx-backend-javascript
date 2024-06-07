ecb7f70744ccbeeb53bde2432f143dfbdec0e26c
oumaima <oumaimanaanaapro@gmail.com>
2024-06-07 12:02:01 +0100

    10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,6 @@
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
-    throw new Error('Cannot Process');
+    throw new Error('Cannot process');
   }
   for (const [key, value] of map) {
     if (value === 1) {
