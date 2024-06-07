aaee472cab2db2c5fbd2bda1ce3c1701a11aba09
oumaima <oumaimanaanaapro@gmail.com>
2024-06-07 12:51:43 +0100

    100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,9 @@
 export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
-  let value = weakMap.get(endpoint);
-  if (value === undefined) {
-    value = 0;
-  }
-  weakMap.set(endpoint, value + 1);
+  let value = weakMap.get(endpoint) || 0;
+  value += 1;
   if (value >= 5) {
     throw new Error('Endpoint load is high');
   }
+  weakMap.set(endpoint, value);
 }
