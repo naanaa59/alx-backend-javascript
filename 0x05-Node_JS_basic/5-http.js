0ad0cb64b0040c31b72d5d9c5d5dff567260e0fa
oumaima <oumaimanaanaapro@gmail.com>
2024-07-23 22:44:39 +0100

    5

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const csStudents = [];
+    const sweStudents = [];
+
+    for (let i = 1; i < lines.length; i += 1) {
+      const fields = lines[i].split(',').map((field) => field.trim());
+
+      const firstname = fields[0];
+      const field = fields[3];
+
+      if (firstname && field) {
+        if (field === 'CS') {
+          csStudents.push(firstname);
+        } else if (field === 'SWE') {
+          sweStudents.push(firstname);
+        }
+      }
+    }
+
+    const totalStudents = csStudents.length + sweStudents.length;
+
+    return (
+      `Number of students: ${totalStudents}\n`
+      + `Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}\n`
+      + `Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`
+    );
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+const app = http.createServer(async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    try {
+      const result = await countStudents(process.argv[2]);
+      const finalResult = `This is the list of our students\n${result}`;
+
+      res.end(finalResult);
+    } catch (error) {
+      res.writeHead(500, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(error));
+    }
+  }
+});
+
+app.listen(1245, '127.0.0.1');
+
+module.exports = app;
