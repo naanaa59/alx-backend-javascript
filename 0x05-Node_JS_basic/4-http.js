47e2e2ab79aedd49a9c1b7dd8820d445263469ea
oumaima <oumaimanaanaapro@gmail.com>
2024-07-23 17:04:06 +0100

    4

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245, '127.0.0.1');
+
+module.exports = app;
