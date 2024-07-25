06c4733569a913a27827f96cb7bea720aba0fff9
oumaima <oumaimanaanaapro@gmail.com>
2024-07-25 19:07:56 +0100

    0

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe("calculateNumber", function() {
+  it('should return the sum of rounded a = 1 and b = 3', function() {
+    assert.equal(calculateNumber(1, 3), 4);
+  });
+  it('should return the sum of rounded a = 1 and b = 3.7', function() {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  });
+  it('should return the sum of rounded a = 1.2 and b = 3.7', function() {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+  it('should return the sum of rounded a = -1.4 and b = -3.7', function() {
+    assert.equal(calculateNumber(-1.4, -3.6), -5);
+  });
+  it('should return the sum of rounded a = 1.5 and b = -2.5', function() {
+    assert.equal(calculateNumber(1.5, -2.5), 0);
+  });
+  it('should handle zero correctly', function() {
+    assert.equal(calculateNumber(0, 0), 0);
+    assert.equal(calculateNumber(0, 1.5), 2);
+    assert.equal(calculateNumber(1.5, 0), 2);
+    assert.equal(calculateNumber(0, -1.5), -1);
+  });
+});
