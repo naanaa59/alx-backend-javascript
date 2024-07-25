b7a53c19a887ecf0c261fc25b4fcef06d17c4432
oumaima <oumaimanaanaapro@gmail.com>
2024-07-25 19:39:59 +0100

    0 1

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -17,6 +17,9 @@ describe("calculateNumber", function() {
   it('should return the sum of rounded a = 1.5 and b = -2.5', function() {
     assert.equal(calculateNumber(1.5, -2.5), 0);
   });
+  it('should return the sum of rounded a = -2.5 and b = -2.5', function() {
+    assert.equal(calculateNumber(-2.5, -2.5), -4);
+  });
   it('should handle zero correctly', function() {
     assert.equal(calculateNumber(0, 0), 0);
     assert.equal(calculateNumber(0, 1.5), 2);
